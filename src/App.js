e28d8067a482c535054fb45ce83c691a513eee56
Shun Isii <sisii00.swork@gmail.com>
2021-07-09 22:07:17 +0900

    hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <Switch>
+        <Route path="/hoge">
+          <Hoge />
+        </Route>
+        <Route path="/">
+          <Hello />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Hello() {
+  return (
+    <div>
+      <h1>ハローワールド</h1>
+      <Link to="/hoge">ほげページへ</Link>
+    </div>
+  );
+}
+
+function Hoge() {
+  return (
+    <div>
+      <h1>ほげほげ</h1>
+      <Link to="">トップページへ</Link>
     </div>
   );
 }
